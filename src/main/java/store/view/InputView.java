3a7c61c2e092b686ff7229b5358be0c73097bfc9
coolman0123 <socool4m@naver.com>
2024-11-11 19:10:37 +0900

    feat: 첫 소개말 출력

---
+++ src/main/java/store/view/InputView.java
@@ -0,0 +1,14 @@
+package store.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+    public void printGreetingMessage(){
+        System.out.println("안녕하세요. W편의점입니다.\n"
+                + "현재 보유하고 있는 상품입니다.\n");
+    }
+
+
+
+
+}
