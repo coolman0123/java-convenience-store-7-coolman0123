a169c09ed39e3370d9aed33f42ffd7b88daa8a40
coolman0123 <socool4m@naver.com>
2024-11-11 19:12:21 +0900

    feat: 구매하는 상품과 수량을 입력 받아라

--- src/main/java/store/view/InputView.java
+++ src/main/java/store/view/InputView.java
@@ -8,6 +8,16 @@ public class InputView {
                 + "현재 보유하고 있는 상품입니다.\n");
     }
 
+    public void requestPurchase(){
+        System.out.println("구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1])");
+    }
+
+    public String getPurcahseList(){
+        String purchaseInput = Console.readLine();
+        return purchaseInput;
+    }
+
+
 
 
 
