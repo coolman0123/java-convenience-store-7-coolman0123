265e20eb19d53f02202f021f7b54b7d4dcfc9438
coolman0123 <socool4m@naver.com>
2024-11-11 21:39:12 +0900

    refactor: 변수명 수정

--- src/main/java/store/service/MembershipService.java
+++ src/main/java/store/service/MembershipService.java
@@ -2,16 +2,14 @@ package store.service;
 
 public class MembershipService {
 
-
-    public int goMembershipDiscount(String input, int rp){
-        if(input.equals("Y")){
-            int discoutRp = (int) (rp * 0.30);
-
-            return discoutRp;
+    public int goMembershipDiscount(String input, int rp) {
+        if (input.equals("Y")) {
+            int discountRate = (int) (rp * 0.30);
+            if (discountRate > 8000) {
+                return 8000;
+            }
+            return discountRate;
         }
         return 0;
-
     }
-
-
 }
