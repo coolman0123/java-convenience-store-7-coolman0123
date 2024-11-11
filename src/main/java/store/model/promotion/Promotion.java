6160ab2f38952e803df0afff121253c902050c7a
coolman0123 <socool4m@naver.com>
2024-11-11 19:18:18 +0900

    feat: promtion 정보 객체

---
+++ src/main/java/store/model/promotion/Promotion.java
@@ -0,0 +1,45 @@
+package store.model.promotion;
+
+import java.time.LocalDate;
+
+public class Promotion {
+    private final String name;
+    private final int buy;
+    private final int get;
+    private final LocalDate startDate;
+    private final LocalDate endDate;
+
+    public Promotion(String name, int buy, int get, LocalDate startDate, LocalDate endDate) {
+        this.name = name;
+        this.buy = buy;
+        this.get = get;
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getBuy() {
+        return buy;
+    }
+
+    public int getGet() {
+        return get;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Promotion{name='" + name + "', buy=" + buy + ", get=" + get +
+                ", startDate=" + startDate + ", endDate=" + endDate + "}";
+    }
+}
